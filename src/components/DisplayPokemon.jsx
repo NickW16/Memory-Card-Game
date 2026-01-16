8f950f7909d834ea49cd15b063949c3f439ad9ec
NickW16 <171855220+NickW16@users.noreply.github.com>
2026-01-16 12:13:30 -0300

    basic pokemon api fetch done

---
+++ src/components/DisplayPokemon.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+
+export default function DisplayPokemon({name = "pikachu"}) {
+   const [pokemon, setPokemon] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+
+   useEffect(() => {
+      const fetchPokemon = async () => {
+         try {
+            setLoading(true); //reset loading state
+            setError(null); //clear previous errors
+
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+
+            if (!response.ok) { //if fails to fetch...
+               throw new Error ('Failed to fetch Pokémon');
+            }
+            const data = await response.json();
+            setPokemon(data);
+            setError(null); // clear error on success
+         } catch (err) {
+            setPokemon(null); // clear pokemon on error
+            setError(err.message);
+         } finally {
+            setLoading(false);
+         }
+      };
+      fetchPokemon();
+
+   }, [name]);
+
+   if(loading) return <div>Loading Pokémon...</div>;
+   if(error) return <div>Error, could not fetch API...</div>;
+   
+   return (
+      <div className="pokemon-card">
+         <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
+         <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width={200}
+         />
+      </div>
+   )
+}
+
